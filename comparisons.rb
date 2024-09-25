aaedd51d33948cc993b117d9a5f3092e3f50ee7a
Will Fox <william.macdonald.fox@gmail.com>
2024-09-24 23:04:36 -0700

    Complete Part 1

--- comparisons.rb
+++ comparisons.rb
@@ -18,29 +18,44 @@ string_teachers = "4"
 numberDogs = 0
 
 # EXAMPLE: print the result of the comparison: is number_seachers greater than number_students?
+
 puts "Is number_teachers greater than number_students?", number_teachers > number_students
 # this should print: "Is numberTeachers greater than numberStudents?" false
 
 # YOU DO: print the result of the comparison: is number_teachers less than number_students?
 # this should print: true
+
 puts "Is number_teachers less than number_students?", number_teachers < number_students 
+
 # YOU DO: print the result of the comparison: is number_teachers equal to string_teachers?
 # this should print: false
+
+puts "Is number_teachers equal to number_students", number_teachers == number_students 
 
 # YOU DO: print the result of the comparison: is number_teachers not equal to number_students?
 # this should print: true
 
+puts "Is number_teachers not equal to number_students", number_teachers != number_students
+
 # YOU DO: print the result of the comparison: is number_students greater than or equal to 20?
 # this should print: true
+
+puts "Is number_teachers greater than or equal to 20", number_teachers >= 20
 
 # YOU DO: print the result of the comparison: is number_students greater than or equal to 21?
 # this should print: false
 
+puts "Is number_students greater than or equal to 21", number_students >= 21
+
 # YOU DO: print the result of the comparison: is number_students less than or equal to 20?
 # this should print: true
 
+puts "Is number_students less than or equal to 20", number_students <= 20
+
 # YOU DO: print the result of the comparison: is number_students less than or equal to 21?
 # this should print: true
+
+puts "Is number_students less than or equal to 21", number_students <= 21
 
 
 #-------------------
